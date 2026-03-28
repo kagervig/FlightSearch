846b8a36cfe5fdd8b0be0f8cdc35dada600eb993
kristianallin <kristianallin@aol.com>
2026-03-28 13:39:55 +0700

    updated database manager and airport model to reflect new db data

--- backend/src/main/java/com/kristian/flightsearch/models/Airport.java
+++ backend/src/main/java/com/kristian/flightsearch/models/Airport.java
@@ -5,22 +5,32 @@ public class Airport {
     String code;
     double lat;
     double lon;
-    int runwayLength;
+    int runwayLengthFt;
     int elevation;
     String city;
     String country;
+    String icaoCode;
+    String timezone;
+    double utcOffset;
 
-
-    public Airport(String code, String name, double lat, double lon, int runwayLength, int elevation, String city, String country){
+    public Airport(String code, String name, double lat, double lon, int runwayLengthFt, int elevation, String city, String country) {
         this.name = name;
         this.code = code;
         this.lat = lat;
         this.lon = lon;
-        this.runwayLength = runwayLength;
+        this.runwayLengthFt = runwayLengthFt;
         this.elevation = elevation;
         this.city = city;
         this.country = country;
     }
+
+    public Airport(String code, String name, double lat, double lon, int runwayLengthFt, int elevation, String city, String country, String icaoCode, String timezone, double utcOffset) {
+        this(code, name, lat, lon, runwayLengthFt, elevation, city, country);
+        this.icaoCode = icaoCode;
+        this.timezone = timezone;
+        this.utcOffset = utcOffset;
+    }
+
     public String getName(){
         return this.name;
     }
@@ -36,8 +46,12 @@ public class Airport {
     public int getElevation(){
         return this.elevation;
     }
+    public int getRunwayLengthFt(){
+        return this.runwayLengthFt;
+    }
+    // Alias kept for compatibility until all call sites are updated
     public int getRunwayLength(){
-        return this.runwayLength;
+        return this.runwayLengthFt;
     }
     public String getCity(){
         return this.city;
@@ -45,5 +59,13 @@ public class Airport {
     public String getCountry(){
         return this.country;
     }
-    
+    public String getIcaoCode(){
+        return this.icaoCode;
+    }
+    public String getTimezone(){
+        return this.timezone;
+    }
+    public double getUtcOffset(){
+        return this.utcOffset;
+    }
 }
